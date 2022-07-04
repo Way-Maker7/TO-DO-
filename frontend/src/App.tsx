0e388ea3954eefed6bcd3af940dd9d3cf2d3af2d
Way-Maker7 <Denis_Sabou@gmx.de>
2022-07-04 17:39:36 +0200

    basic template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,58 @@
 import React from 'react'
+import './App.css'
 
 function App() {
 
 
-
-
     return (
         <div>
-           <h1>hallo</h1>
+
+            <div>
+                <h1>TO DO ANWENDUNG</h1>
+            </div>
+
+            <div>
+                <input type="text"/>
+                <input type="text"/>
+                <button>OK</button>
+            </div>
+
+            <div>
+                <div>
+                    Status 1
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>ok</button>
+                        <button>ok</button>
+                        <button>ok</button>
+                    </div>
+                </div>
+
+                <div>
+                    Status 2
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>ok</button>
+                        <button>ok</button>
+                        <button>ok</button>
+                    </div>
+                </div>
+
+                <div>
+                    Status 3
+                    <div>
+                        <p>Task</p>
+                        <p>Beschreibung</p>
+                        <button>ok</button>
+                        <button>ok</button>
+                        <button>ok</button>
+                    </div>
+                </div>
+
+            </div>
+
         </div>
     );
 }
